170b7642933f58c6446d3c0b87769c01814cb6d7
John Huang <johnhuang1225@gmail.com>
2017-07-25 11:37:40 +0800

    add extension

---
+++ extension.js
@@ -0,0 +1,8 @@
+module.exports = {
+    exts: [
+        {name: 'john', ext: '116'},
+        {name: 'yulin', ext: '115'},
+        {name: 'alistar', ext: '126'},
+        {name: 'tommy', ext: '117'}
+    ]
+};
--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 var linebot = require('linebot');
 var express = require('express');
-var _ = require('underscore');
+// var _ = require('underscore');
+var extension = require('./extension');
 
 var port = process.env.PORT || 3000;
 
@@ -13,16 +14,8 @@ var bot = linebot({
 bot.on('message', function(event) {
     if (event.message.type=='text') {
         var msg = event.message.text;
-        var regex = /^[1-9]{1}[0-9]*$/;
-        var replyMsg = '你說什麼我不知道，請你輸入數字';
-        if (regex.test(msg)) {
-            if (0<=msg && msg<=500){
-                replyMsg = `換人民幣 ${msg}, 匯率：4.50`;
-            }
-            else {
-                replyMsg = `換人民幣 ${msg}, 匯率：4.49`;
-            }
-        }
+        var replyMsg = rateAnswer(msg);
+        console.log(extension);
 
         event.reply(replyMsg).then(function(data) {
             console.log(`replyMsg: ${replyMsg}`);
@@ -42,3 +35,19 @@ app.post('/', linebotParser);
 app.listen(port, function() {
     console.log(`linebot now running on port ${port}`);
 });
+
+
+
+function rateAnswer(msg) {
+    var regex = /^[1-9]{1}[0-9]*$/;
+    var replyMsg = '你說什麼我不知道，請你輸入數字';
+    if (regex.test(msg)) {
+        if (0<=msg && msg<=500){
+            replyMsg = `換人民幣 ${msg}, 匯率：4.50`;
+        }
+        else {
+            replyMsg = `換人民幣 ${msg}, 匯率：4.49`;
+        }
+    }
+    return replyMsg;
+}
